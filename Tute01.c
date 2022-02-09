c4fbbe3051d9f49e175681d0342d8dc0547bc374
it21215506 <99312516+IT21215506@users.noreply.github.com>
2022-02-09 06:10:15 +0000

    Completed 1

--- Tute01.c
+++ Tute01.c
@@ -5,6 +5,23 @@
 #include <stdio.h>
 
 int main() {
+
+ 
+ float mark1,mark2;
+ float tot,avg;
+
+//user inputs
+ printf("Input the mark of subject 1- ");
+ scanf("%f",&mark1);
+
+ printf("\nInput the mark of subject 2- ");
+ scanf("%f",&mark2);
+
+ tot=mark1+mark2;//calculating the total
+ avg=tot/2;//calculating the average
+
+//prinying the average
+ printf("\nAverage marks of subjects- %2.f",avg);
   
   return 0;
 }
